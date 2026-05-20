c6fb2b1addc846f9c1376fddbd409b1f147b2dae
P214218 <skt.p214218@partner.sktelecom.com>
2026-05-20 16:51:33 +0900

    chapter. process practice

--- 10_Dev/OSTEP/practice/프로세스/practice1.c
+++ 10_Dev/OSTEP/practice/프로세스/practice1.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main () {
   int x = 1;
-  int pid = fork();
+  pid_t pid = fork();
 
   if(pid < 0) {
     // fork 실패
     return -1;
   } else if (pid == 0) {
-    printf("자식 프로세스 실행 중... x의 값은 %d", x);
+    printf("자식 프로세스 실행 중... x의 값은 %d \n", x);
   } else {
-    wait();
-    printf("부모 프로세스 실행 중 자식 프로세스 PID: %d, x의 값은 %d", pid, x);
+    wait(NULL);
+    printf("부모 프로세스 실행 중 자식 프로세스 PID: %d, x의 값은 %d \n", pid, x);
   }
 
   return 0;
