6240735ccea7a88c0d85b90d3eb0f27c3ab09bd4
P214218 <skt.p214218@partner.sktelecom.com>
2026-06-29 17:46:48 +0900

    algorithm

---
+++ 10_Dev/문병로교수/ch4_정렬_연습문제/insertion_sort.test.js
@@ -0,0 +1,23 @@
+/**
+ * @description
+ *
+ * @param
+ * @param
+ * */
+function solution () {
+  return 0;
+}
+
+describe("INSERTION SORT TEST", () => {
+  test("예제 1", () => {
+    expect(solution()).toEqual([]);
+  });
+
+  test("예제 2", () => {
+    expect(solution()).toEqual([]);
+  });
+
+  test("예제 3", () => {
+    expect(solution()).toEqual([]);
+  });
+});
