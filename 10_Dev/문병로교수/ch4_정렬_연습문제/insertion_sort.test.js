67a1ae4dcd923700ed6879fb537f77225be90fdf
P214218 <skt.p214218@partner.sktelecom.com>
2026-07-16 17:23:58 +0900

    sort algorithm

--- 10_Dev/문병로교수/ch4_정렬_연습문제/insertion_sort.test.js
+++ 10_Dev/문병로교수/ch4_정렬_연습문제/insertion_sort.test.js
@@ -1,11 +1,10 @@
 /**
- * @description
- *
- * @param
- * @param
+ * @description 삽입 정렬 구현
+ * @param N {number} 배열의 길이
+ * @param Arr {Array<number>}
  * */
-function solution () {
-  return 0;
+function solution (N, Arr) {
+  return Arr;
 }
 
 describe("INSERTION SORT TEST", () => {
