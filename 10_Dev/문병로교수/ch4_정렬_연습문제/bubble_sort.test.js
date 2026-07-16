297658bce7a302ed5e0c28531b1b7be31f6bd679
P214218 <skt.p214218@partner.sktelecom.com>
2026-07-16 17:05:43 +0900

    sort algorithm

--- 10_Dev/문병로교수/ch4_정렬_연습문제/bubble_sort.test.js
+++ 10_Dev/문병로교수/ch4_정렬_연습문제/bubble_sort.test.js
@@ -1,26 +1,32 @@
 /**
- * @description `
- *
- *
- * `
- * @param {number} N
- * @param {number} K
+ * @description 버블 정렬 구현
+ * @param {number} N  배열의 길이
  * @param {Array<number>} Arr
  * */
-function solution(N, K, Arr) {
-  return 0;
+function solution(N, Arr) {
+  for(let i = 0; i < N - 1; i++) {
+    let swapped = false;
+    for(let j = 0; j < N - i - 1; j++) {
+      if(Arr[j] > Arr[j+1]) {
+        swapped = true;
+        [Arr[j], Arr[j+1]] = [Arr[j + 1], Arr[j]]
+      }
+    }
+    if(!swapped) break;
+  }
+  return Arr;
 }
 
 describe("버블 정렬 TEST", () => {
   test("예제 1번", () => {
-    expect(solution(0, 0, [])).toEqual([]);
+    expect(solution(0, [])).toEqual([]);
   });
 
   test("예제 2번", () => {
-    expect(solution(0, 0, [])).toEqual([]);
+    expect(solution(0,[])).toEqual([]);
   });
 
   test("예제 3번", () => {
-    expect(solution(0, 0, [])).toEqual([]);
+    expect(solution(0,[])).toEqual([]);
   });
 });
--- 10_Dev/문병로교수/ch4_정렬_연습문제/selection_sort.test.js
+++ 10_Dev/문병로교수/ch4_정렬_연습문제/selection_sort.test.js
@@ -1,34 +1,38 @@
 /**
- * @description `
- *  문제: K회 선택 정렬 후 배열 상태 구하기
- *  선택 정렬을 수행한다.
- *  앞에서부터 k번 최솟값을 선택해 제자리에 둔 뒤 배열 상태를 출력하세요.
- *  같은 최솟값이 여러 개라면 가장 앞에 있는 값을 선택합니다.
- *
- *  [입력 제한]
- *  2 <= N <= 5000
- *  1 <= K <= N-1
- *  -1,000,000 <= ai <= 1,000,000
- * `
+ * @description 선택 정렬 후 배열 상태
  * @param {number} N 입력 배열의 길이
- * @param {number} K
  * @param {Array<number>} Arr
- * @return k회 선택 후 배열
+ * @return 선택 정렬 후 배열
  * */
-function solution(N, K, Arr) {
+function solution(N, Arr) {
+  for(let i = 0; i < N - 1; i++) {
+    let MAX_NUM_IDX = 0;
+    let LAST_ELEM = Arr[N-i-1];
+
+    for(let j = 0; j < N - i; j++) {
+      if(Arr[j] > Arr[MAX_NUM_IDX]) {
+        MAX_NUM_IDX = j;
+      }
+    }
+
+    if(MAX_NUM_IDX !== N - i - 1) {
+      Arr[N-i-1] = Arr[MAX_NUM_IDX];
+      Arr[MAX_NUM_IDX] = LAST_ELEM;
+    }
+  }
   return Arr;
 }
 
 decribe("선택 정렬 TEST", () => {
   test("예제 1번", () => {
-    expect(solution(5, 2, [64, 25, 12, 22, 11])).toEqual([11, 12, 25, 22, 64]);
+    expect(solution(5, [64, 25, 12, 22, 11])).toEqual([11, 12, 25, 22, 64]);
   });
 
   test("예제 2번", () => {
-    expect(solution(6, 3, [3, 1, 2, 1, 5, 4])).toEqual([1, 1, 2, 3, 5, 4]);
+    expect(solution(6, [3, 1, 2, 1, 5, 4])).toEqual([1, 1, 2, 3, 5, 4]);
   });
 
   test("예제 3번", () => {
-    expect(solution(4, 2, [-3, -1, -2, 0])).toEqual([-3, -1, -2, 0]);
+    expect(solution(4,  [-3, -1, -2, 0])).toEqual([-3, -1, -2, 0]);
   });
 });
