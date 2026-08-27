7011a9c00bca7acdac28c53c945ac9637d7a3930
P214218 <skt.p214218@partner.sktelecom.com>
2026-08-27 17:55:03 +0900

    merge sort

---
+++ 10_Dev/문병로교수/ch4_정렬_연습문제/merge_sort.test.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Array<number>} F
+ * @param {Array<number>} B
+ */
+function merge(F, B) {
+  return [];
+}
+
+/**
+ * @param {Array<number>} A
+ * */
+function mergeSort(A) {
+  return A;
+}
+
+describe("병합 정렬 테스트 ", () => {
+  it("테스트 예제 1", () => {
+    expect(mergeSort([])).toEqual([]);
+  });
+
+  it("테스트 예제 2", () => {
+    expect(mergeSort([])).toEqual([]);
+  });
+
+  it("테스트 예제 3", () => {
+    expect(mergeSort([])).toEqual([]);
+  });
+});
